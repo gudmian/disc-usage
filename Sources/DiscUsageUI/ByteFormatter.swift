2f9121e6ea9fe11f5a6f85a874d0516d0f7d6483
gudmian <d.guba@vk.team>
2026-07-08 11:43:17 +0300

    feat: форматтер размеров, цвета категорий и проверка Full Disk Access

--- Sources/DiscUsageUI/ByteFormatter.swift
+++ Sources/DiscUsageUI/ByteFormatter.swift
@@ -4,6 +4,7 @@ public enum ByteFormatter {
     public static func string(_ bytes: Int64) -> String {
         let formatter = ByteCountFormatter()
         formatter.countStyle = .file
+        formatter.allowsNonnumericFormatting = false  // «0 байт», а не «Zero KB»
         return formatter.string(fromByteCount: bytes)
     }
 }
