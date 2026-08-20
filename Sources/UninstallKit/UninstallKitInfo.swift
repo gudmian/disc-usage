de5567d1f8917d00293cdf4b3285af9ffe351d78
gudmian <d.guba@vk.team>
2026-08-20 18:02:47 +0300

    chore: scaffold UninstallKit target

---
+++ Sources/UninstallKit/UninstallKitInfo.swift
@@ -0,0 +1,3 @@
+public enum UninstallKitInfo {
+    public static let version = "0.1.0"
+}
---
+++ Tests/UninstallKitTests/SmokeTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import UninstallKit
+
+@Test func packageCompiles() {
+    #expect(UninstallKitInfo.version == "0.1.0")
+}
